988812c34dbb601e2a929046e7ba14d93c54dc50
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2024-06-09 14:03:08 +0530

    git initialised for the day

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ function App() {
   //   'Have breakfast'
   // ]
 
-  const [todos, setTodos] = useState(['Go to the gym',
-    'Freshen up',
-    'Have breakfast'
-  ])
+  //NO NEED TO USE THE TASKS IN THE ARRAY SINCE WE ARE USING THE STATE AND ADDING THE TASKS TO THE ARRAY USING THE STATE
+  // const [todos, setTodos] = useState(['Go to the gym',
+  //   'Freshen up',
+  //   'Have breakfast'
+  // ])
+  const [todos, setTodos] = useState([])
+
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
